c24aecafc7067e89e04fb643b119629629590598
swejangit <swejanbonagiri123@gmail.com>
2026-06-05 15:40:38 +0530

    fixed db path

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import sqlite3
 import os
 
-DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cashiq.db')
+DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database/cashiq.db')
 
 def get_db_connection():
     conn = sqlite3.connect(DB_PATH)
